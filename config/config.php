e8d6917fff759152d3507be318e2248a3cd96aba
RONY ANDERSON ALVES DA SILVA <65975236+RonyCode@users.noreply.github.com>
2020-08-27 17:39:26 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -6,9 +6,9 @@ define(
     [
         'host' => 'smtp.gmail.com',
         'port' => '587',
-        'user' => 'ronyandersonpc@gmail.com',
-        'passwd' => '170286para',
+        'user' => '*********',
+        'passwd' => '**********',
         'from_name' => 'Espaço Educar',
-        'from_email' => 'ronyandersonpc@gmail.com'
+        'from_email' => '*********@gmail.com'
     ]
 );
